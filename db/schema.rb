d356fb9e31a9e9eb0fbbf02c7ed9561108730e1e
Nick Donias <ndonias@gmail.com>
2015-01-30 14:41:59 -0500

    added followers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130155026) do
+ActiveRecord::Schema.define(version: 20150130183902) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20150130155026) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "location"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "folowed_id"
   end
 
   create_table "users", force: :cascade do |t|
--- main.rb
+++ main.rb
@@ -3,17 +3,18 @@ require 'sinatra/activerecord'
 require 'bundler/setup'
 require 'rack-flash'
 require_relative './models.rb'
-enable :sessions
-use Rack::Flash, :sweep
+
+# enable :sessions
+# use Rack::Flash, :sweep
 
 set :database, "sqlite3:data.sqlite3"
-set :sessions, true
+# set :sessions, true
 
 get '/' do
 	erb :signin
 end
 
-post '/sign-in' do
+post '/signin' do
 	@user = User.where(username: params[:username]).first
 	if @user.password == params[:password]
 		redirect '/my_profile'
@@ -22,14 +23,49 @@ post '/sign-in' do
 	end
 end
 
+get '/feed' do
+	erb :feed
+end
+
+get '/my_account' do
+	erb :my_account
+end
+
+get '/my_profile' do
+	erb :my_profile
+end
+
+get '/profiles' do
+	erb :profiles
+end
+
+get '/signup' do
+	erb :signup
+end
+
+get '/add_post' do
+	erb :add_post
+end
+
+post '/add_post' do
+	Post.create(params[:post])
+	flash[:notice]="New post created."
+	redirect to '/'
+end
+
 def current_user
 	if session[:user_id]
 		@current_user = User.find(session[:user_id])
 	end
 end
 
-result = Posts.find(:all, :order => "id desc", :limit =>5)
-
-	while ! result.empty?
-		@feeds= result.pop
+get '/follow/:id' do
+	@relationship = Relationship.new(follower_id: current_user.id, 
+									 followed_id: params[:id])
+	if @relationship.save
+		flash[:notice]="You've successfully followed"
+	else
+		flash[:alert]="There was a problem."
+	end
+	redirect back
 end
